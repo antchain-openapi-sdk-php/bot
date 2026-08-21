6b42725a6ac0bc90d05b61ff2e9366475928b353
SDK Builder <sdk-builder@alipay.com>
2026-08-21 10:32:10 +0800

    Generated SDK for BOT.

---
+++ src/Models/ListIotagentSessionResponse.php
@@ -0,0 +1,90 @@
+<?php
+
+// This file is auto-generated, don't edit it. Thanks.
+namespace AntChain\BOT\Models;
+
+use AlibabaCloud\Tea\Model;
+
+use AntChain\BOT\Models\SessionInfo;
+
+class ListIotagentSessionResponse extends Model {
+    protected $_name = [
+        'reqMsgId' => 'req_msg_id',
+        'resultCode' => 'result_code',
+        'resultMsg' => 'result_msg',
+        'sessionList' => 'session_list',
+    ];
+    public function validate() {}
+    public function toMap() {
+        $res = [];
+        if (null !== $this->reqMsgId) {
+            $res['req_msg_id'] = $this->reqMsgId;
+        }
+        if (null !== $this->resultCode) {
+            $res['result_code'] = $this->resultCode;
+        }
+        if (null !== $this->resultMsg) {
+            $res['result_msg'] = $this->resultMsg;
+        }
+        if (null !== $this->sessionList) {
+            $res['session_list'] = [];
+            if(null !== $this->sessionList && is_array($this->sessionList)){
+                $n = 0;
+                foreach($this->sessionList as $item){
+                    $res['session_list'][$n++] = null !== $item ? $item->toMap() : $item;
+                }
+            }
+        }
+        return $res;
+    }
+    /**
+     * @param array $map
+     * @return ListIotagentSessionResponse
+     */
+    public static function fromMap($map = []) {
+        $model = new self();
+        if(isset($map['req_msg_id'])){
+            $model->reqMsgId = $map['req_msg_id'];
+        }
+        if(isset($map['result_code'])){
+            $model->resultCode = $map['result_code'];
+        }
+        if(isset($map['result_msg'])){
+            $model->resultMsg = $map['result_msg'];
+        }
+        if(isset($map['session_list'])){
+            if(!empty($map['session_list'])){
+                $model->sessionList = [];
+                $n = 0;
+                foreach($map['session_list'] as $item) {
+                    $model->sessionList[$n++] = null !== $item ? SessionInfo::fromMap($item) : $item;
+                }
+            }
+        }
+        return $model;
+    }
+    // 请求唯一ID，用于链路跟踪和问题排查
+    /**
+     * @var string
+     */
+    public $reqMsgId;
+
+    // 结果码，一般OK表示调用成功
+    /**
+     * @var string
+     */
+    public $resultCode;
+
+    // 异常信息的文本描述
+    /**
+     * @var string
+     */
+    public $resultMsg;
+
+    // session 信息
+    /**
+     * @var SessionInfo[]
+     */
+    public $sessionList;
+
+}
